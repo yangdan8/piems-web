ea8faf38966cf4a75ee9e53050487e68ce19bd9d
yangd <yangd@pmac.com.cn>
2018-12-27 10:41:42 +0800

    init

---
+++ src/components/HelloWorld.ts
@@ -0,0 +1,9 @@
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class HelloWorld extends Vue {
+  @Prop() private msg!: string;
+  showMsg() {
+    alert(this.msg);
+  }
+}
